87f3f610bc867a7451a2b11d737edd9b50750531
Ditah K <101201113+ditahk@users.noreply.github.com>
2023-01-18 18:50:32 -0500

    Update helloword.sh

--- helloword.sh
+++ helloword.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This is a sample hello world application
+# Please use it for free
+
 echo "This is ${USER}'s Hello World App"
 echo ""
 echo "You are logged in as ${USER} & located at ${PWD}"
